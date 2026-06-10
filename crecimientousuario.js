6cd0cc9d746c6d54cbfcf6179bb46b0f22fe8666
Schattenheartz <aquice2001@gmail.com>
2026-06-10 12:21:30 -0500

    modified:   crecimientousuario.js modified:   estilos.css modified:   index.html

--- crecimientousuario.js
+++ crecimientousuario.js
@@ -1,1 +1,38 @@
 // Crecimiento de Usuarios
+function simular() {
+    let cantidadInicial = parseInt(document.getElementById("cantidadInicial").value);
+    let metaUsuarios = parseInt(document.getElementById("metaUsuarios").value);
+    let cantidadActual = cantidadInicial;
+    let dias = 0;
+// Validaciones - Restricciones
+    if (
+        cantidadInicial <= 0 ||
+        metaUsuarios <= 0 ||
+        cantidadInicial >= metaUsuarios ||
+        (metaUsuarios - cantidadInicial) > 4000
+    ) {
+        document.getElementById("resultado").innerHTML =
+            "Los datos ingresados no son correctos.";
+        return;
+    }
+// Calcular 
+    while (cantidadActual < metaUsuarios) {
+        dias++;
+        if (cantidadActual < 1000) {
+            cantidadActual += 50;
+        }
+        else if (cantidadActual < 3000) {
+            cantidadActual += 80;
+        }
+        else {
+            cantidadActual += 120;
+        }
+
+        if (dias % 7 === 0) {
+            cantidadActual -= 100;
+        }
+    }
+// Resultado
+    document.getElementById("resultado").innerHTML =
+        `La plataforma alcanza la meta en ${dias} días.`;
+}
